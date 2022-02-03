c793b4c991d49a7fd192315c8e4c63fc15b2703d
Mr.Steelcan <mr.steelcan@gmail.com>
2022-02-03 11:53:35 +0200

    .

--- airtable.js
+++ airtable.js
@@ -1,8 +1,8 @@
 import Airtable from 'airtable';
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_ID
-);
+const base = new Airtable({
+  apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
+}).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID);
 
 const minifiedRecords = (records) => {
   return records.map((record) => minifyRecord(record));
