981e901215a9f3e09764ed56222352fa2979cb3e
Mr.Steelcan <mr.steelcan@gmail.com>
2022-02-03 12:12:50 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,17 +1,14 @@
 module.exports = {
   author: {
-    email: 'kamilcancelik5757@gmail.com',
+    email: 'mr.steelcan@gmail.com',
     name: 'Kamilcan Çelik',
   },
   social: {
-    unsplash: 'https://unsplash.com/@ademilter',
-    vsco: 'https://vsco.co/adem/gallery',
-    instagram: 'https://instagram.com/ademilter',
-    twitter: 'https://twitter.com/ademilter',
-    medium: 'https://medium.com/@ademilter',
-    github: 'https://github.com/ademilter',
-    youtube: 'https://youtube.com/ademilter',
-    feyz: 'https://t.me/feyzli',
-    superpeer: 'https://superpeer.com/adem',
+    unsplash: 'https://unsplash.com/',
+    instagram: 'https://www.instagram.com/cristiano/',
+    twitter: 'https://twitter.com/',
+    medium: 'https://medium.com/',
+    github: 'https://github.com/KamilcanCelik',
+    youtube: 'https://youtube.com/',
   },
 };
